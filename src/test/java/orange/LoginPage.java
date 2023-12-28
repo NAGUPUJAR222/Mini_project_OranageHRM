dba1d61488a9c25717727728a7473c71c85a3e45
Nagaraj <28nagarajpujar@gmail.com>
2023-12-28 18:03:12 +0530

    first round of commit......

---
+++ src/test/java/orange/LoginPage.java
@@ -0,0 +1,28 @@
+package orange;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginPage {
+	
+	@Test
+	public void verifyLogin()
+	{
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		driver.findElement(By.name("username")).sendKeys("Admin");
+		driver.findElement(By.name("password")).sendKeys("admin123");
+		driver.findElement(By.xpath("//button[.=' Login ']")).click();
+		driver.close();
+	}
+
+}
