053ad8a75d82b9a045d359c6395465122e45834d
Nagaraj <28nagarajpujar@gmail.com>
2024-01-05 11:13:18 +0530

    SECOND COMMIT

---
+++ src/main/java/MINI_PROJECT_ORANGEHRM/MINI_PROJECT_ORANGEHRM/App.java
@@ -0,0 +1,13 @@
+package MINI_PROJECT_ORANGEHRM.MINI_PROJECT_ORANGEHRM;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/orange/LoginPage.java
@@ -0,0 +1,55 @@
+package orange;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginPage {
+	
+	@Parameters("Browser")
+	@Test
+	public void verifyLogin(String browserName)
+	{
+		System.out.println("Paramter value is " +browserName);
+		WebDriver driver=null;
+		if (browserName.contains("chrome"))
+		{
+			WebDriverManager.chromedriver().setup();
+			 driver=new ChromeDriver();
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		}
+		 
+		else if (browserName.contains("firefox"))
+		{
+			WebDriverManager.firefoxdriver().setup();
+			driver=new FirefoxDriver();
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		}
+		
+		else if (browserName.contains("ie"))
+		{
+			WebDriverManager.iedriver().setup();
+			driver=new InternetExplorerDriver();
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		}
+
+		
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		driver.findElement(By.name("username")).sendKeys("Admin");
+		driver.findElement(By.name("password")).sendKeys("admin123");
+		driver.findElement(By.xpath("//button[.=' Login ']")).click();
+		driver.close();
+	}
+
+}
